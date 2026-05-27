a7179310663f9501efa5c1367f7673f0283b3cc8
mhafezan <your_email@example.com>
2026-05-27 12:18:39 -0400

    Text generation with GPT2 added.

--- 2_HuggingFace_API/1_Local_Hardware_Pipeline_Text_Generation_GPT2.py
+++ 2_HuggingFace_API/1_Local_Hardware_Pipeline_Text_Generation_GPT2.py
@@ -1,9 +1,14 @@
 from transformers import pipeline
 
-gpt2_pipeline = pipeline(task="text-generation", model="openai-community/gpt2")
+gpt2_pipeline = pipeline(
+    task="text-generation",
+    model="openai-community/gpt2")
 
-# Generate three text outputs with a maximum length of 10 tokens
-results = gpt2_pipeline("What if AI", max_new_tokens=10, num_return_sequences=2)
+results = gpt2_pipeline(
+    "Hi, how are you doing today?",
+    max_new_tokens=20,
+    num_return_sequences=2,
+    pad_token_id=gpt2_pipeline.tokenizer.eos_token_id)
 
 for result in results:
-    print(result['generated_text'])
+    print(f"\n{result['generated_text']}\n")
